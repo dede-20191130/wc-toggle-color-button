3214ae703c2fa23853c4acdd56efdd88214aaa74
dede_20191130 <1044adad@gmail.com>
2021-10-26 13:52:44 +0900

    first commit

---
+++ my-app/dist/toggle-color-button-component.js
@@ -0,0 +1,7 @@
+"use strict";
+// test code
+window.addEventListener("DOMContentLoaded", function (event) {
+    var p = document.createElement("p");
+    p.textContent = "testcode";
+    document.body.append(p);
+});
---
+++ my-app/src/toggle-color-button-component.ts
@@ -0,0 +1,7 @@
+// test code
+window.addEventListener("DOMContentLoaded", (event) => {
+    const p = document.createElement("p");
+    p.textContent = "testcode";
+    document.body.append(p);
+
+})
